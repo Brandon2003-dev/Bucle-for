81a96d5604f313f11e2902accc360841b1dca4c2
Brandon Emerson <111790942+Brandon2003-dev@users.noreply.github.com>
2023-03-08 21:22:56 -0600

    Add files via upload

---
+++ Ejercicio for/Bucle For.cs
@@ -0,0 +1,18 @@
+# bucle for.
+
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Introduce un número entero: ");
+        int altura = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j <= i; j++) {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
